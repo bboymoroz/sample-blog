b8c8ea7b6f876234ad13048f1ae2e5fe0998031a
Морозов Иван Александрович <ivan.morozov@tele2.ru>
2020-01-22 11:47:25 +0600

    Save contact on submit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -3,7 +3,15 @@ class ContactsController < ApplicationController
   def new
   end
 
-  def crete
+  def create
+    @contact = Contact.new(contact_params)
+    @contact.save
   end
-  
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:email, :message)
+  end
+
 end
