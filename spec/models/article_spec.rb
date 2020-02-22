641b910ec28be78a3fcb6a3a2f34ac521c8b96a7
bboymoroz <bboymoroz@mail.ru>
2020-02-22 22:44:50 +0600

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -10,4 +10,14 @@ describe Article do
   describe "associatons" do    
     it { should have_many :comments }  
   end
+
+
+  describe "#subject" do
+    it "returns the article title" do
+      article = create(:article, title: 'Lorem Ipsum')
+
+      # assert
+      expect(article.subject).to eq 'Lorem Ipsum'
+    end
+  end
 end
